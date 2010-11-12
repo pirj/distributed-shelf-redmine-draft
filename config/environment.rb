acc6e2d28e91f6b3c6833fb4752fbf491643e20b
Phil Pirj <pirj@mail.ru>
2010-11-12 23:11:18 +0300

    moving init earlier

--- config/environment.rb
+++ config/environment.rb
@@ -17,14 +17,6 @@ rescue LoadError
   # Not available
 end
 
-require 'dshelf'
-DistributedShelf::config = {
-  :distributed_path => ['/files', '/plugin_assets'],
-  :storage_url => ENV['DISTRIBUTED_SHELF_URL']
-}
-Dir.mkdir '/plugin_assets' unless File.exists?('/plugin_assets')
-Dir.mkdir '/files' unless File.exists?('/files')
-
 Rails::Initializer.run do |config|
   # Settings in config/environments/* take precedence those specified here
   
